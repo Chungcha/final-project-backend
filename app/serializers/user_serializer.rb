9325fa6e39104f5f3d9e2a3c5fec410da4c54c71
chungcha <charlieschung@gmail.com>
2019-12-18 10:53:48 -0500

    serialized meetups to past/future

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :id, :username, :first_name, :last_name, :wait_queue, :meetups
+  attributes :id, :username, :first_name, :last_name, :wait_queue, :future_meetups, :past_meetups
+
+  def future_meetups
+    object.meetups.filter{|meetup|Time.parse(meetup.datetime).future?}
+  end
+
+  def past_meetups 
+    object.meetups.filter{|meetup|Time.parse(meetup.datetime).past?}
+  end
 
 end
